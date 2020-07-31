9049fa5b96e031fa9b932fe225a143885aa8f105
jorgegwd <jorge.rodriguez.r@gmail.com>
2020-07-31 10:50:26 -0500

    Animation fix

--- src/components/home.js
+++ src/components/home.js
@@ -33,7 +33,7 @@ class Home extends Component {
             <div className="home">
                 <div className="main">
                     <div className="text-center">
-                        <ScrollAnimation animateIn="fadeInUp">
+                        <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
                             <h1>Coding the <strong>future</strong></h1>
                             <h2>Connect with the online business</h2>
                         </ScrollAnimation>
@@ -49,24 +49,24 @@ class Home extends Component {
                                 key={index}
                                 className={index !== 0 ? "slider-content double" : "slider-content single" }
                             >
-                                <ScrollAnimation animateIn="fadeInLeft">
+                                <ScrollAnimation animateIn="fadeInLeft" animateOnce={true}>
                                     <div className="image">
                                         <img src={item.image} alt="" />
                                     </div>
                                 </ScrollAnimation>
-                                <ScrollAnimation animateIn="fadeInRight">
+                                <ScrollAnimation animateIn="fadeInRight" animateOnce={true}>
                                     <div className="animation">
                                         <img src={item.animation} alt="" />
                                     </div>
                                 </ScrollAnimation>
                                 <div className="inner">
-                                    <ScrollAnimation animateIn="fadeInDown">
+                                    <ScrollAnimation animateIn="fadeInDown" animateOnce={true}>
                                         <div className="title">
                                             <img className="softars-icon" src={item.image} alt="" />
                                             <h1>{item.title}</h1>
                                         </div>
                                     </ScrollAnimation>
-                                    <ScrollAnimation animateIn="fadeInUp">
+                                    <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
                                         <p>{item.description}</p>
                                     </ScrollAnimation>
                                 </div>
@@ -79,13 +79,13 @@ class Home extends Component {
                         <div className="left">
                             <div className="text-center">
                                 <div className="description">
-                                    <ScrollAnimation animateIn="fadeInDown">
+                                    <ScrollAnimation animateIn="fadeInDown" animateOnce={true}>
                                         <div className="title">
                                             <img src={require('../images/title-icon-system.svg')} className="softars-icon" alt="" />
                                             <h1>Make it work</h1>
                                         </div>
                                     </ScrollAnimation>
-                                    <ScrollAnimation animateIn="fadeInUp">
+                                    <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
                                         <p>We use technology that allows us to develop whatever you have in mind.</p>
                                     </ScrollAnimation>
                                 </div>
@@ -93,22 +93,22 @@ class Home extends Component {
                         </div>
                         <div className="right">
                             <div className="text-center">
-                                <ScrollAnimation animateIn="fadeInRight">
+                                <ScrollAnimation animateIn="fadeInRight" animateOnce={true}>
                                 <img src={require('../images/brand-icon-angular.svg')} className="brand-icon" alt="" />
                                 </ScrollAnimation>
-                                <ScrollAnimation animateIn="fadeInRight">
+                                <ScrollAnimation animateIn="fadeInRight" animateOnce={true}>
                                 <img src={require('../images/brand-icon-react.svg')} className="brand-icon" alt="" />
                                 </ScrollAnimation>
-                                <ScrollAnimation animateIn="fadeInRight">
+                                <ScrollAnimation animateIn="fadeInRight" animateOnce={true}>
                                 <img src={require('../images/brand-icon-nodejs.svg')} className="brand-icon" alt="" />
                                 </ScrollAnimation>
-                                <ScrollAnimation animateIn="fadeInRight">
+                                <ScrollAnimation animateIn="fadeInRight" animateOnce={true}>
                                 <img src={require('../images/brand-icon-bootstrap.svg')} className="brand-icon" alt="" />
                                 </ScrollAnimation>
-                                <ScrollAnimation animateIn="fadeInRight">
+                                <ScrollAnimation animateIn="fadeInRight" animateOnce={true}>
                                 <img src={require('../images/brand-icon-javascript.svg')} className="brand-icon" alt="" />
                                 </ScrollAnimation>
-                                <ScrollAnimation animateIn="fadeInRight">
+                                <ScrollAnimation animateIn="fadeInRight" animateOnce={true}>
                                 <img src={require('../images/brand-icon-larave.svg')} className="brand-icon" alt="" />
                                 </ScrollAnimation>
                             </div>
@@ -119,29 +119,29 @@ class Home extends Component {
                     <div className="section">
                         <div className="inerbox">
                             <div className="text-center">
-                                <ScrollAnimation animateIn="fadeInDown">
+                                <ScrollAnimation animateIn="fadeInDown" animateOnce={true}>
                                     <div className="title">
                                         <img src={require('../images/title-icon-gamedev.svg')} className="softars-icon" alt="" />
                                         <h1>Ready for gaming</h1>
                                     </div>
                                 </ScrollAnimation>
-                                <ScrollAnimation animateIn="fadeInLeft">
+                                <ScrollAnimation animateIn="fadeInLeft" animateOnce={true}>
                                     <p>Get involved with the business of videogames.</p>
                                 </ScrollAnimation>
                                 <ul className="engines">
-                                    <ScrollAnimation animateIn="fadeInRight" delay={0}>
+                                    <ScrollAnimation animateIn="fadeInRight" delay={0} animateOnce={true}>
                                         <li className="grid-item"><img src={require('../images/engine-icon-defold.svg')} className="engine-icon" alt="" /></li>
                                     </ScrollAnimation>
-                                    <ScrollAnimation animateIn="fadeInRight" delay={200}>
+                                    <ScrollAnimation animateIn="fadeInRight" delay={200} animateOnce={true}>
                                         <li className="grid-item"><img src={require('../images/engine-icon-unity.svg')} className="engine-icon" alt="" /></li>
                                     </ScrollAnimation>
-                                    <ScrollAnimation animateIn="fadeInRight" delay={400}>
+                                    <ScrollAnimation animateIn="fadeInRight" delay={400} animateOnce={true}>
                                         <li className="grid-item"><img src={require('../images/engine-icon-corona.svg')} className="engine-icon" alt="" /></li>
                                     </ScrollAnimation>
-                                    <ScrollAnimation animateIn="fadeInRight" delay={600}>
+                                    <ScrollAnimation animateIn="fadeInRight" delay={600} animateOnce={true}>
                                         <li className="grid-item"><img src={require('../images/engine-icon-unreal.svg')} className="engine-icon" alt="" /></li>
                                     </ScrollAnimation>
-                                    <ScrollAnimation animateIn="fadeInRight" delay={800}>
+                                    <ScrollAnimation animateIn="fadeInRight" delay={800} animateOnce={true}>
                                         <li className="grid-item"><img src={require('../images/engine-icon-godot.svg')} className="engine-icon" alt="" /></li>
                                     </ScrollAnimation>
                                 </ul>
@@ -156,12 +156,12 @@ class Home extends Component {
                         <div className="right">
                             <div className="text-center">
                                 <div className="description">
-                                    <ScrollAnimation animateIn="fadeInUp">
+                                    <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
                                         <div className="title">
                                             <h1>Custom experience</h1>
                                         </div>
                                     </ScrollAnimation>
-                                    <ScrollAnimation animateIn="fadeInUp" delay={200}>
+                                    <ScrollAnimation animateIn="fadeInUp" delay={200} animateOnce={true}>
                                         <p>“There’s no such thing as a dumb idea, only poorly focused. Here on Softars we work to ensure your idea stays on the right track.”</p>
                                     </ScrollAnimation>
                                 </div>
@@ -171,12 +171,12 @@ class Home extends Component {
                 </div>
                 <div className="follow">
                     <div className="section text-center">
-                        <ScrollAnimation animateIn="fadeInUp">
+                        <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
                             <div className="title">
                                 <h1>Follow us on</h1>
                             </div>
                         </ScrollAnimation>
-                        <ScrollAnimation animateIn="fadeInUp" delay={200}>
+                        <ScrollAnimation animateIn="fadeInUp" delay={200} animateOnce={true}>
                             <div className="">
                                 <a rel="noopener noreferrer" href="jsx-a11y/href-no-hash" target="_blank"><img src={require('../images/social-icon-facebook.svg')} className="social-icon" alt="" /></a>
                                 <a rel="noopener noreferrer" href="jsx-a11y/href-no-hash" target="_blank"><img src={require('../images/social-icon-twitter.svg')} className="social-icon" alt="" /></a>
