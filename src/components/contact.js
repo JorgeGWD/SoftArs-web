24250d28115d7235c379bb0f9b8633e999294fb3
jorgegwd <jorge.rodriguez.r@gmail.com>
2020-09-07 17:25:34 -0500

    Upgrade react, use hook in header, fix scroll animatios

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,98 +1,58 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-//import MenuItem from '@material-ui/core/MenuItem';
-import TextField from '@material-ui/core/TextField';
+import React from 'react'
 
-const styles = theme => ({
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    textField: {
-        marginLeft: theme.spacing.unit,
-        marginRight: theme.spacing.unit,
-        width: 200,
-    },
-    menu: {
-        width: 200,
-    },
-});
+const Contact = () => {
+ 
+    const handleChange = () => {
 
-class TextFields extends React.Component {
-    state = {
-        name: '',
-        multiline: ''
-    };
+    }
 
-    handleChange = name => event => {
-        this.setState({
-            [name]: event.target.value,
-        });
-    };
-
-    render() {
-        const { classes } = this.props;
-        return (
-            <div className="contact">
-                <div className="inbox">
-                    <input type="checkbox" name="" id="icon"/>
-                    <label htmlFor="icon" className="icon">
-                        <img src={require('../images/icon-chat-bubble.svg')} className="email-icon" alt="Email" />
-                    </label>
-                    <div className="box">
-                        <p><span>Hello, we’d love to hear your ideas</span> <br/> Let us know how can we help</p>
-                        <img src={require('../images/times.svg')} className="close-icon" alt="Close" />
-                        <form className={classes.container} noValidate autoComplete="off">
-                            <TextField
-                                id="name"
-                                label="Enterprise"
-                                className={classes.textField}
-                                value={this.state.name}
-                                onChange={this.handleChange('name')}
-                                margin="normal"
-                            />
-                            <TextField
-                                id="country"
-                                label="Country"
-                                className={classes.textField}
-                                value={this.state.country}
-                                onChange={this.handleChange('country')}
-                                margin="normal"
-                            />
-                            <TextField
-                                id="email"
-                                label="Your email"
-                                className={classes.textField}
-                                value={this.state.email}
-                                onChange={this.handleChange('email')}
-                                margin="normal"
-                            />
-                            <TextField
-                                id="multiline-static"
-                                label="Your message"
-                                multiline
-                                rows="4"
-                                defaultValue=""
-                                className={classes.textField}
-                                margin="normal"
-                            />
-                        </form>
-                        <button className="send">Send</button>
-                    </div>
-                </div>
-                <div className="confirmation">
-                    <h2>Great!</h2>
-                    <p>Thank you for contacting us <br/>You’ll hear from us soon enough</p>
-                    <button className="send">Okay</button>
+    return (
+        <div className="contact">
+            <div className="inbox">
+                <input type="checkbox" name="" id="icon"/>
+                <label htmlFor="icon" className="icon">
+                    <img src={require('../images/icon-chat-bubble.svg')} className="email-icon" alt="Email" />
+                </label>
+                <div className="box">
+                    <p><span>Hello, we’d love to hear your ideas</span> <br/> Let us know how can we help</p>
+                    <img src={require('../images/times.svg')} className="close-icon" alt="Close" />
+                    <form noValidate autoComplete="off">
+                        <input
+                            id="name"
+                            label="Enterprise"
+                            name="name"
+                            onChange={handleChange}
+                        />
+                        <input
+                            id="country"
+                            label="Country"
+                            name="country"
+                            onChange={handleChange}
+                        />
+                        <input
+                            id="email"
+                            label="Your email"
+                            name="email"
+                            onChange={handleChange}
+                        />
+                        <textarea
+                            id="multiline-static"
+                            label="Your message"
+                            rows="4"
+                            defaultValue=""
+                            onChange={handleChange}
+                        />
+                    </form>
+                    <button className="send">Send</button>
                 </div>
             </div>
-        );
-    }
+            <div className="confirmation">
+                <h2>Great!</h2>
+                <p>Thank you for contacting us <br/>You’ll hear from us soon enough</p>
+                <button className="send">Okay</button>
+            </div>
+        </div>
+    )
 }
 
-TextFields.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(TextFields);
+export default Contact
--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef } from 'react'
 
-class Header extends Component {
-    render() {
-        return (
-            <div className="header">
-                <header className="App-header">
-                    <a href=""><img src={require('../images/logo-softars.svg')} className="App-logo" alt="SoftArs" /></a>
-                </header>
-            </div>
-        );
+const Header = () => {
+
+    const [ sticky, setSticky ] = useState({ isSticky: false, offset: 0 });
+
+    const ref = useRef(null);
+
+    const handleScroll = () => {
+        if (window.pageYOffset > 0 ) {
+          setSticky({ isSticky: true });
+        } else {
+          setSticky({ isSticky: false, offset: 0 });
+        }
+    };
+
+      useEffect(() => {
+        var header = ref.current.getBoundingClientRect();
+        const handleScrollEvent = () => {
+          handleScroll(header.top, header.height)
     }
+       
+        window.addEventListener('scroll', handleScrollEvent);
+       
+        return () => {
+          window.removeEventListener('scroll', handleScrollEvent);
+        };
+    }, []);
+
+    return (
+        <header className={`header${sticky.isSticky ? ' sticky' : ''}`} ref={ref}>
+            <a href="jsx-a11y/anchor-is-valid"><img src={require('../images/logo-softars.svg')} className="App-logo" alt="SoftArs" /></a>
+        </header>
+    )
 }
 
-export default Header;
+export default Header
